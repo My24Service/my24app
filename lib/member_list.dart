2898f09dcbf85728cca028347a773514cb935953
Richard Jansen <richard@pedroja.tech>
2020-05-30 19:47:39 +0200

    split up main.dart

---
+++ lib/member_list.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'models.dart';
+import 'member_detail.dart';
+
+
+Future<Members> fetchMembers() async {
+  final response = await http.get('https://demo.my24service-dev.com/member/list-public/');
+
+  if (response.statusCode == 200) {
+    return Members.fromJson(json.decode(response.body));
+  }
+
+  throw Exception('Failed to load members');
+}
+
+class MembersList extends Object {
+  Future<Members> members;
+
+  _setCompanycode(String companycode) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    print('storing $companycode');
+    await prefs.setString('companycode', companycode);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Members',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Choose member'),
+        ),
+        body: Center(
+          child: FutureBuilder<Members>(
+              future: members,
+              builder: (context, snapshot) {
+                print(snapshot.data);
+                if (snapshot.data == null) {
+                  return Container(
+                      child: Center(
+                          child: Text("Loading...")
+                      )
+                  );
+                } else {
+                  return ListView.builder(
+                      itemCount: snapshot.data.results.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  snapshot.data.results[index].companylogo
+                              ),
+                            ),
+                            title: Text(snapshot.data.results[index].name),
+                            subtitle: Text(snapshot.data.results[index].companycode),
+                            onTap: () {
+                              _setCompanycode(snapshot.data.results[index].companycode);
+                              Navigator.push(context,
+                                  new MaterialPageRoute(builder: (context) =>
+                                      MemberPage(snapshot.data.results[index]))
+                              );
+                            } // onTab
+                        );
+                      } // itemBuilder
+                  );
+                } // else
+              } // builder
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/models.dart
@@ -0,0 +1,63 @@
+class Members {
+  final int count;
+  final String next;
+  final String previous;
+  final List<MemberPublic> results;
+
+  Members({
+    this.count,
+    this.next,
+    this.previous,
+    this.results,
+  });
+
+  factory Members.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['results'] as List;
+    List<MemberPublic> results = list.map((i) => MemberPublic.fromJson(i)).toList();
+
+    return Members(
+        count: parsedJson['count'],
+        next: parsedJson['next'],
+        previous: parsedJson['previous'],
+        results: results
+    );
+  }
+}
+
+class MemberPublic {
+  final String companycode;
+  final String companylogo;
+  final String name;
+  final String address;
+  final String postal;
+  final String city;
+  final String countryCode;
+  final String tel;
+  final String email;
+
+  MemberPublic({
+    this.companycode,
+    this.companylogo,
+    this.name,
+    this.address,
+    this.postal,
+    this.city,
+    this.countryCode,
+    this.tel,
+    this.email,
+  });
+
+  factory MemberPublic.fromJson(Map<String, dynamic> parsedJson) {
+    return MemberPublic(
+      companycode: parsedJson['companycode'],
+      companylogo: parsedJson['companylogo'],
+      name: parsedJson['name'],
+      address: parsedJson['address'],
+      postal: parsedJson['postal'],
+      city: parsedJson['city'],
+      countryCode: parsedJson['country_code'],
+      tel: parsedJson['tel'],
+      email: parsedJson['email'],
+    );
+  }
+}
