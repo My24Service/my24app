d60f9390c954205d158986082f3ee07af2f1e9c0
Richard Jansen <richard@pedroja.tech>
2020-05-31 12:25:46 +0200

    first routing setup

---
+++ lib/routes.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'routing_constants.dart';
+import 'main.dart';
+import 'login.dart';
+
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case HomeViewRoute:
+      return MaterialPageRoute(builder: (context) => My24App());
+    case LoginViewRoute:
+      return MaterialPageRoute(builder: (context) => LoginPageWidget());
+    default:
+      return MaterialPageRoute(builder: (context) => My24App());
+  }
+}
