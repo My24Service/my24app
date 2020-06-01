fd3630e1f17b81c4cf82656de2e44f474b9e3d1c
Richard Jansen <richard@pedroja.tech>
2020-06-01 15:11:23 +0200

    fixed failing test (SharedPreferences.setMockInitialValues)

--- test/member_list_test.dart
+++ test/member_list_test.dart
@@ -2,20 +2,23 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:matcher/matcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:my24app/member_list.dart';
 import 'package:my24app/models.dart';
-import 'package:my24app/main.dart';
+
 
 // Create a MockClient using the Mock class provided by the Mockito package.
 // Create new instances of this class in each test.
 class MockClient extends Mock implements http.Client {}
 
-
 main() {
   group('fetchMembers', () {
     test('returns a Members list if the http call completes successfully', () async {
       final client = MockClient();
+      SharedPreferences.setMockInitialValues({
+        'apiBaseUrl': 'my24service-dev.com'
+      });
 
       // Use Mockito to return a successful response when it calls the
       // provided http.Client.
