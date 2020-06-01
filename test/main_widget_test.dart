5a89a0a8b1fd0b92fb4de9919e4143d5390bddfc
Richard Jansen <richard@pedroja.tech>
2020-06-01 15:11:23 +0200

    fixed failing test (SharedPreferences.setMockInitialValues)

--- test/main_widget_test.dart
+++ test/main_widget_test.dart
@@ -8,12 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:my24app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(My24App());
-
-  });
 }
