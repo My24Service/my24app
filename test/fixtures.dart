8e9184baa1e2b474bc5d44263ed090a3af127ee3
Richard Jansen <richard@pedroja.tech>
2020-06-03 21:36:22 +0200

    added fixtures file for tests

---
+++ test/fixtures.dart
@@ -0,0 +1,30 @@
+var userInfoEngineer = '''{
+   "user": {
+      "id": 3,
+      "email": "planningstormy@gmail.com",
+      "username": "rene",
+      "engineer": {
+        "address": "Metaalweg 4",
+        "postal": "3751LS",
+        "city": "BUNSCHOTEN-SPAKENBURG",
+        "country_code": "NL",
+        "mobile": "+31625026601",
+        "email_tablet": null,
+        "passport": null,
+        "vca": "false",
+        "cost_price": null,
+        "license_plate": null,
+        "inspection_date_car": null,
+        "cost_price_car": null,
+        "inspection_date_tools": null,
+        "cost_price_tools": null,
+        "remarks": null
+      },
+      "full_name": "Rene Buitenhuis",
+      "last_login": "09/03/2020 11:58",
+      "date_joined": "27/09/2016 19:20",
+      "first_name": "Rene",
+      "last_name": "Buitenhuis"
+      },
+  "submodel": "engineer"
+}''';
