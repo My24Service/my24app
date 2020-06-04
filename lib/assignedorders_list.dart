f0af045feb20efe02feae4fed4605eb2ec548a73
Richard Jansen <richard@pedroja.tech>
2020-06-04 11:46:12 +0200

    added a basic AssignedOrders page and a fixture for testing

---
+++ lib/assignedorders_list.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+import 'models.dart';
+
+
+class AssignedOrdersListWidget extends StatelessWidget {
+  final Engineer engineer;
+
+  AssignedOrdersListWidget(this.engineer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Orders for ${engineer.firstName}')),
+      body: Center(child: Text('My Page!')),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
