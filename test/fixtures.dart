2517ac0b0fcdb7a7a1e61a66007487ca4ce6c9ba
Richard Jansen <richard@pedroja.tech>
2020-06-04 11:46:12 +0200

    added a basic AssignedOrders page and a fixture for testing

--- test/fixtures.dart
+++ test/fixtures.dart
@@ -1,4 +1,5 @@
-var userInfoEngineer = '''{
+var userInfoEngineer = '''
+{
    "user": {
       "id": 3,
       "email": "planningstormy@gmail.com",
@@ -28,3 +29,73 @@ var userInfoEngineer = '''{
       },
   "submodel": "engineer"
 }''';
+
+var assignedOrdersList = '''
+{
+    "user": "e",
+    "assignedorders": [
+        {
+            "id": 6899,
+            "engineer": {
+                "id": 22,
+                "user": {
+                    "id": 29,
+                    "email": "humanications@gmail.com",
+                    "username": "e",
+                    "last_login": null,
+                    "date_joined": "2020-06-04T11:09:36.259020",
+                    "first_name": "Test",
+                    "last_name": "User"
+                },
+                "address": null,
+                "postal": null,
+                "city": null,
+                "country_code": "NL",
+                "mobile": null
+            },
+            "student_user": null,
+            "order": {
+                "id": 6196,
+                "customer_id": "1454",
+                "order_id": "19128",
+                "service_number": "",
+                "order_reference": "",
+                "order_type": "Storing",
+                "customer_remarks": null,
+                "description": null,
+                "start_date": "09/06/2020",
+                "start_time": null,
+                "end_date": "09/06/2020",
+                "end_time": null,
+                "order_date": "09/06/2020",
+                "last_status": "toegewezen aan e",
+                "order_name": "GLS",
+                "order_address": "Businesspark 407",
+                "order_postal": "6181MD",
+                "order_city": "Stein",
+                "order_country_code": "NL",
+                "order_tel": "",
+                "order_mobile": "",
+                "order_email": "",
+                "order_contact": "Dhr. Paul de Vos  06-11311355",
+                "last_status_full": "04/06/2020 11:31 toegewezen aan e",
+                "required_users": 1,
+                "created": "2020/06/04 11:30",
+                "total_price_purchase": "0.00",
+                "total_price_selling": "0.00"
+            },
+            "started": "-",
+            "ended": "-",
+            "activity_totals": {
+                "work_total": "00:00",
+                "travel_to_total": "00:00",
+                "travel_back_total": "00:00",
+                "distance_to_total": 0,
+                "distance_back_total": 0
+            },
+            "workorders_count": 0,
+            "rating": 0
+        }
+    ]
+}
+''';
