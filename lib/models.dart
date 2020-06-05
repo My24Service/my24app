466e75440458c0292134137d84a2d905a2d78247
Richard Jansen <richard@pedroja.tech>
2020-06-05 13:54:21 +0200

    added models for assignedorders list

--- lib/models.dart
+++ lib/models.dart
@@ -184,7 +184,7 @@ class EngineerProperty {
   }
 }
 
-class Engineer {
+class EngineerUser {
   final int id;
   final String email;
   final String username;
@@ -193,7 +193,7 @@ class Engineer {
   final String lastName;
   EngineerProperty engineer;
 
-  Engineer({
+  EngineerUser({
     this.id,
     this.email,
     this.username,
@@ -203,8 +203,8 @@ class Engineer {
     this.engineer,
   });
 
-  factory Engineer.fromJson(Map<String, dynamic> parsedJson) {
-    return Engineer(
+  factory EngineerUser.fromJson(Map<String, dynamic> parsedJson) {
+    return EngineerUser(
       id: parsedJson['id'],
       email: parsedJson['email'],
       username: parsedJson['username'],
@@ -216,3 +216,152 @@ class Engineer {
   }
 }
 
+class Order {
+  final int id;
+  final String customerId;
+  final String orderId;
+  final String serviceNumber;
+  final String orderReference;
+  final String orderType;
+  final String customerRemarks;
+  final String description;
+  final String startDate;
+  final String startTime;
+  final String endDate;
+  final String endTime;
+  final String orderDate;
+  final String lastStatus;
+  final String orderName;
+  final String orderAddress;
+  final String orderPostal;
+  final String orderCity;
+  final String orderCountryCode;
+  final String orderTel;
+  final String orderMobile;
+  final String orderEmail;
+  final String orderContact;
+  final String lastStatusFull;
+  final int requireUsers;
+  final String created;
+  final String totalPricePurchase;
+  final String totalPriceSelling;
+
+  Order({
+    this.id,
+    this.customerId,
+    this.orderId,
+    this.serviceNumber,
+    this.orderReference,
+    this.orderType,
+    this.customerRemarks,
+    this.description,
+    this.startDate,
+    this.startTime,
+    this.endDate,
+    this.endTime,
+    this.lastStatus,
+    this.orderName,
+    this.orderAddress,
+    this.orderPostal,
+    this.orderCity,
+    this.orderCountryCode,
+    this.orderTel,
+    this.orderMobile,
+    this.orderContact,
+    this.lastStatusFull,
+    this.requireUsers,
+    this.created,
+    this.totalPricePurchase,
+    this.totalPriceSelling,
+    this.orderDate,
+    this.orderEmail,
+  });
+
+  factory Order.fromJson(Map<String, dynamic> parsedJson) {
+    return Order(
+      id: parsedJson['id'],
+      customerId: 'customer_id',
+      orderId: parsedJson['order_id'],
+      serviceNumber: parsedJson['service_number'],
+      orderReference: parsedJson['order_reference'],
+      orderType: parsedJson['order_type'],
+      customerRemarks: parsedJson['customer_remarks'],
+      description: parsedJson['description'],
+      startDate: parsedJson['start_date'],
+      startTime: parsedJson['start_time'],
+      endDate: parsedJson['end_date'],
+      endTime: parsedJson['end_time'],
+      lastStatus: parsedJson['last_status'],
+      orderName: parsedJson['order_name'],
+      orderAddress: parsedJson['order_address'],
+      orderPostal: parsedJson['order_postal'],
+      orderCity: parsedJson['order_city'],
+      orderCountryCode: parsedJson['order_country_code'],
+      orderTel: parsedJson['order_tel'],
+      orderMobile: parsedJson['order_mobile'],
+      orderContact: parsedJson['order_contact'],
+      lastStatusFull: parsedJson['last_status_full'],
+      requireUsers: parsedJson['required_users'],
+      created: parsedJson['created'],
+      totalPricePurchase: parsedJson['total_price_purchase'],
+      totalPriceSelling: parsedJson['total_price_selling'],
+      orderEmail: parsedJson['order_email'],
+      orderDate: parsedJson['order_date'],
+    );
+  }
+}
+
+class AssignedOrder {
+  final int id;
+  final int engineer;
+  final int studentUser;
+  final Order order;
+  final String started;
+  final String ended;
+
+  AssignedOrder({
+    this.id,
+    this.engineer,
+    this.studentUser,
+    this.order,
+    this.started,
+    this.ended,
+  });
+
+  factory AssignedOrder.fromJson(Map<String, dynamic> parsedJson) {
+    return AssignedOrder(
+      id: parsedJson['id'],
+      order: Order.fromJson(parsedJson['order']),
+      engineer: parsedJson['engineer'],
+      studentUser: parsedJson['student_user'],
+      started: parsedJson['started'],
+      ended: parsedJson['ended'],
+    );
+  }
+}
+
+class AssignedOrders {
+  final int count;
+  final String next;
+  final String previous;
+  final List<AssignedOrder> results;
+
+  AssignedOrders({
+    this.count,
+    this.next,
+    this.previous,
+    this.results,
+  });
+
+  factory AssignedOrders.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['results'] as List;
+    List<AssignedOrder> results = list.map((i) => AssignedOrder.fromJson(i)).toList();
+
+    return AssignedOrders(
+        count: parsedJson['count'],
+        next: parsedJson['next'],
+        previous: parsedJson['previous'],
+        results: results
+    );
+  }
+}
