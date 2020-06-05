63ddd3b0de966cb7c0c6280ee7fda7c28f841be1
Richard Jansen <richard@pedroja.tech>
2020-06-05 13:55:58 +0200

    assignedorders fixture to new structure

--- test/fixtures.dart
+++ test/fixtures.dart
@@ -87,72 +87,48 @@ var userInfoEngineer = '''
   "submodel": "engineer"
 }''';
 
-var assignedOrdersList = '''
-{
-    "user": "e",
-    "assignedorders": [
+var assignedOrdersList = {
+    "next": null,
+    "previous": null,
+    "count": 1,
+    "num_pages": 1,
+    "results": [
         {
-            "id": 6899,
-            "engineer": {
-                "id": 22,
-                "user": {
-                    "id": 29,
-                    "email": "humanications@gmail.com",
-                    "username": "e",
-                    "last_login": null,
-                    "date_joined": "2020-06-04T11:09:36.259020",
-                    "first_name": "Test",
-                    "last_name": "User"
-                },
-                "address": null,
-                "postal": null,
-                "city": null,
-                "country_code": "NL",
-                "mobile": null
-            },
+            "id": 6915,
+            "engineer": 19,
             "student_user": null,
             "order": {
-                "id": 6196,
-                "customer_id": "1454",
-                "order_id": "19128",
+                "id": 6208,
+                "customer_id": "1002",
+                "order_id": "19140",
                 "service_number": "",
                 "order_reference": "",
                 "order_type": "Storing",
                 "customer_remarks": null,
                 "description": null,
-                "start_date": "09/06/2020",
+                "start_date": "15/06/2020",
                 "start_time": null,
-                "end_date": "09/06/2020",
+                "end_date": "15/06/2020",
                 "end_time": null,
-                "order_date": "09/06/2020",
-                "last_status": "toegewezen aan e",
-                "order_name": "GLS",
-                "order_address": "Businesspark 407",
-                "order_postal": "6181MD",
-                "order_city": "Stein",
+                "order_date": "15/06/2020",
+                "last_status": "toegewezen aan Emmertje1989@hotmail.com",
+                "order_name": "Abbott Laboratories B.V.",
+                "order_address": "Rieteweg 21",
+                "order_postal": "8000AP",
+                "order_city": "ZWOLLE",
                 "order_country_code": "NL",
-                "order_tel": "",
+                "order_tel": "038-4279740",
                 "order_mobile": "",
                 "order_email": "",
-                "order_contact": "Dhr. Paul de Vos  06-11311355",
-                "last_status_full": "04/06/2020 11:31 toegewezen aan e",
+                "order_contact": "elvert (nieuwe cp)",
+                "last_status_full": "05/06/2020 13:23 toegewezen aan Emmertje1989@hotmail.com",
                 "required_users": 1,
-                "created": "2020/06/04 11:30",
+                "created": "2020/06/05 13:22",
                 "total_price_purchase": "0.00",
                 "total_price_selling": "0.00"
             },
             "started": "-",
-            "ended": "-",
-            "activity_totals": {
-                "work_total": "00:00",
-                "travel_to_total": "00:00",
-                "travel_back_total": "00:00",
-                "distance_to_total": 0,
-                "distance_back_total": 0
-            },
-            "workorders_count": 0,
-            "rating": 0
+            "ended": "-"
         }
     ]
-}
-''';
+};
