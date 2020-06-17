0ed639c7970cf4ae79982bea44b790b538fed583
Richard Jansen <richard@pedroja.tech>
2020-06-17 11:10:33 +0200

    removed missing import

--- lib/member_detail.dart
+++ lib/member_detail.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 
 import 'models.dart';
