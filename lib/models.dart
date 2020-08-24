2ed9aa13c1de60c12c47cfe3eb0e1754e2f5575a
Richard Jansen <richard@pedroja.tech>
2020-08-24 09:16:43 +0200

    added AssignedOrderDocument models

--- lib/models.dart
+++ lib/models.dart
@@ -717,3 +717,55 @@ class AssignedOrderActivities {
     );
   }
 }
+
+class AssignedOrderDocument {
+  final int id;
+  final int assignedOrderId;
+  final String name;
+  final String description;
+  final String document;
+
+  AssignedOrderDocument({
+    this.id,
+    this.assignedOrderId,
+    this.name,
+    this.description,
+    this.document,
+  });
+
+  factory AssignedOrderDocument.fromJson(Map<String, dynamic> parsedJson) {
+    return AssignedOrderDocument(
+      id: parsedJson['id'],
+      assignedOrderId: parsedJson['assigned_order'],
+      name: parsedJson['name'],
+      description: parsedJson['description'],
+      document: parsedJson['document'],
+    );
+  }
+}
+
+class AssignedOrderDocuments {
+  final int count;
+  final String next;
+  final String previous;
+  final List<AssignedOrderDocument> results;
+
+  AssignedOrderDocuments({
+    this.count,
+    this.next,
+    this.previous,
+    this.results,
+  });
+
+  factory AssignedOrderDocuments.fromJson(Map<String, dynamic> parsedJson) {
+    var list = parsedJson['results'] as List;
+    List<AssignedOrderDocument> results = list.map((i) => AssignedOrderDocument.fromJson(i)).toList();
+
+    return AssignedOrderDocuments(
+      count: parsedJson['count'],
+      next: parsedJson['next'],
+      previous: parsedJson['previous'],
+      results: results,
+    );
+  }
+}
