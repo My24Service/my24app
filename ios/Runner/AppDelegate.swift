317d8abe42d7f152852ceed92c0ffa989f165821
Richard Jansen <richard@pedroja.tech>
2020-10-10 22:19:35 +0200

    AppDelegate application func merge

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,16 +3,12 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegatem,UIResponder,UIApplicationDelegate {
-  func application(_ application:UIApplication,didFinishLaunchingWithOptions launchOptions:[UIApplication.LaunchOptionsKey:Any]?)->Bool{
-      // Other intialization code…
-      UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
-
-      return true
-  }
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
