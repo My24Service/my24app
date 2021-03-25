027e76ab36c6015ddd627b9797bd0a5b29baeaa1
Richard Jansen <richard@pedroja.tech>
2021-03-25 13:35:47 +0100

    createSnackBar now pre-2.x ready

--- lib/utils.dart
+++ lib/utils.dart
@@ -1117,6 +1117,8 @@ showDeleteDialog(String title, String content, BuildContext context, Function de
 
 
 createSnackBar(BuildContext context, String content) {
+  const String isOld = String.fromEnvironment('OLD');
+
   final snackBar = SnackBar(
     content: Text(content),
     // action: SnackBarAction(
@@ -1129,5 +1131,9 @@ createSnackBar(BuildContext context, String content) {
 
   // Find the ScaffoldMessenger in the widget tree
   // and use it to show a SnackBar.
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  if (isOld.length > 0) {
+    Scaffold.of(context).showSnackBar(snackBar);
+  } else {
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
 }
