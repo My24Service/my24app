e04c3f041f14e035a3b3433d7840944d5630da7d
Richard Jansen <richard@pedroja.tech>
2021-04-07 20:59:04 +0200

    home page & widgets refactor WIP

---
+++ lib/home/widgets/landingpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LandingPageWidget extends StatelessWidget {
+  LandingPageWidget({
+    Key key,
+  }):super(key: key);
+
+  bool _doSkip = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
