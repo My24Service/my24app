2ce4fe7aed5853b6fdd1f35a756d789ec51b0956
Richard Jansen <richard@pedroja.tech>
2021-04-08 20:38:54 +0200

    work on test

--- test/home_widgets_landingpage_test.dart
+++ test/home_widgets_landingpage_test.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:my24app/home/widgets/landingpage.dart';
+import 'package:my24app/member/blocs/fetch_bloc.dart';
+
+
+Widget createBlocProviderForWidget({Widget child}) {
+  return BlocProvider(
+    create: (_) => FetchMemberBloc(),
+    child: child
+  );
+}
 
 void main() {
   testWidgets('LandingPageWidget loads', (WidgetTester tester) async {
-    // LandingPageWidget(doSkip: state.doSkip, memberPk: state.memberPk)
-    await tester.pumpWidget(LandingPageWidget(
-    	doSkip: true, memberPk: 1));
+    await tester.pumpWidget(
+      createBlocProviderForWidget(child: LandingPageWidget(
+      doSkip: true, memberPk: 1))
+    );
+
+    tester.pump(Duration(milliseconds: 1000));
   });
 }
