e205322142292bd6f912cbe2cdc485d9f4d69d34
Richard Jansen <richard@pedroja.tech>
2021-11-05 13:21:57 +0100

    added UpgradeAlert component

--- lib/home/pages/home.dart
+++ lib/home/pages/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:upgrader/upgrader.dart';
+
 import 'package:my24app/core/utils.dart';
 import 'package:my24app/core/widgets/widgets.dart';
 import 'package:my24app/home/blocs/preferences_bloc.dart';
@@ -106,15 +108,17 @@ class BuildLandingPageScaffold extends StatelessWidget {
           title: Text(doSkip ? 'main.app_bar_title_continue'.tr() : 'main.app_bar_title'.tr()),
           centerTitle: true,
         ),
-        body: Container(
-            child: Column(
-              children: [
-                BlocProvider(
-                    create: (BuildContext context) => createBloc,
-                    child: LandingPageWidget(doSkip: doSkip)
-                )
-              ],
-            )
+        body: UpgradeAlert(
+          child: Container(
+              child: Column(
+                children: [
+                  BlocProvider(
+                      create: (BuildContext context) => createBloc,
+                      child: LandingPageWidget(doSkip: doSkip)
+                  )
+                ],
+              )
+          )
         )
     );
   }
