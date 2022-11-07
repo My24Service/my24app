ca2019dea2905878f5c225f7c001f89e67fc7273
Richard Jansen <richard@pedroja.tech>
2022-11-07 12:48:18 +0100

    updated release notes

--- lib/login/widgets/widgets.dart
+++ lib/login/widgets/widgets.dart
@@ -107,7 +107,7 @@ class _LoginViewState extends State<LoginView> {
   }
 
   _passwordReset () async {
-    final url = await utils.getUrl('/company/users/password-reset/#users/reset-password');
+    final url = await utils.getUrl('/frontend/#/reset-password');
     launch(url.replaceAll('/api', ''));
   }
 
