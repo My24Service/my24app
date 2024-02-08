a7a5bed12efc26cec1b91cb9c37a12846d0580cb
Richard Jansen <richard@pedroja.tech>
2024-02-08 14:14:49 +0100

    bugfix in customer order history

--- lib/order/models/order/models.dart
+++ lib/order/models/order/models.dart
@@ -354,7 +354,7 @@ class CustomerHistoryOrder extends BaseModel {
   final String? orderType;
   final String? orderReference;
   final String? workorderPdfUrl;
-  final String? workorderPdfUrlPartner;
+  // final String? workorderPdfUrlPartner;
   final String? lastStatus;
   final String? lastStatusFull;
   final List<Orderline>? orderLines;
@@ -366,7 +366,7 @@ class CustomerHistoryOrder extends BaseModel {
     this.orderType,
     this.orderReference,
     this.workorderPdfUrl,
-    this.workorderPdfUrlPartner,
+    // this.workorderPdfUrlPartner,
     this.lastStatus,
     this.lastStatusFull,
     this.orderLines,
@@ -383,7 +383,7 @@ class CustomerHistoryOrder extends BaseModel {
       orderType: parsedJson['order_type'],
       orderReference: parsedJson['order_reference'],
       workorderPdfUrl: parsedJson['workorder_pdf_url'],
-      workorderPdfUrlPartner: parsedJson['workorder_pdf_url_partner'],
+      // workorderPdfUrlPartner: parsedJson['workorder_pdf_url_partner'],
       lastStatus: parsedJson['last_status'],
       lastStatusFull: parsedJson['last_status_full'],
       orderLines: orderLines,
